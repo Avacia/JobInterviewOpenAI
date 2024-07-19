6708449590c34b5bf2b19cd028257453784648c1
Jeanny Gere van Erkel <157359660+jeanz888@users.noreply.github.com>
2024-07-19 21:55:55 +1200

    updated style

---
+++ frontend/OpenAIInterviewer/src/styles/JobTitle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-bottom: 1.5rem;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;

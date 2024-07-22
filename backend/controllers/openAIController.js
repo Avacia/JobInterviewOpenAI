595677b6ecf62cfde96a7d8a2d9bac4a9e1dfbb3
Avacia <yenw@missionreadyhq.com>
2024-07-22 19:34:40 +1200

    adding comment for matias

--- backend/controllers/openAIController.js
+++ backend/controllers/openAIController.js
@@ -1,7 +1,7 @@
 module.exports.interviewConversation = async(req, res) => {
 
     try{
-
+        /* Matias fill in the code here */
     }
     catch(error){
         res.status(500).send("Internal Server Error")
